8756c6917e26973f5acbe1d19e43586e87a84abf
emshen6 <312637@niuitmo.ru>
2023-11-17 14:39:14 +0300

    helpers

---
+++ src/helpers/skillsList.js
@@ -0,0 +1,32 @@
+const skillsList = [
+  {
+    title: "Data science and Machine Learning",
+    skills:
+      "Python, SQL, Numpy, Pandas, Sklearn, Keras, OpenCV, Classic prediction models, CNN, Transfer learning",
+    learnt: 60,
+  },
+  {
+    title: "Java programming",
+    skills:
+      "Java, Stream API, Spring CDI, Design patterns, MVC template, ORM Hibernate",
+    learnt: 70,
+  },
+  {
+    title: "Web",
+    skills:
+      "HTML, CSS, JS, AJAX, jQuery, MVC, Java Servlets and JSP, JSF, Java EE, React JS, REST API",
+    learnt: 40,
+  },
+  {
+    title: "Low-level programming",
+    skills: "Assembly, C",
+    learnt: 20,
+  },
+  {
+    title: "Minecraft modding",
+    skills: "Java",
+    learnt: 90,
+  },
+];
+
+export { skillsList };
