75938adea0750d85d3cecdcf58cc8317f259cff5
emshen6 <parfenovao752@gmail.com>
2024-01-16 11:07:31 +0300

    отображение информации о проекте внутри тега div

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -5,10 +5,10 @@ import { NavLink } from "react-router-dom";
 const Project = (props) => {
   return (
     <NavLink to={`/project/${props.index}`}>
-      <li className="project">
+      <div className="project">
         <img src={props.img} alt={props.title} className="project__img" />
         <h3 className="project__title">{props.title}</h3>
-      </li>
+      </div>
     </NavLink>
   );
 };
