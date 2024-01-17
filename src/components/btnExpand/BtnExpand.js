0c8f9cf6a35fca13ffbfb99be3687ffc6bae1d18
emshen6 <parfenovao752@gmail.com>
2024-01-17 13:40:34 +0300

    анимация progress bar перенесена в css

--- src/components/btnExpand/BtnExpand.js
+++ src/components/btnExpand/BtnExpand.js
@@ -1,5 +1,5 @@
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
 import "./style.css";
 
 const BtnExpand = (props) => {
@@ -7,25 +7,6 @@ const BtnExpand = (props) => {
     <FaChevronDown className="expandIcon" />
   );
   const [isComponentVisible, setComponentVisibility] = useState(false);
-  const [progress, setProgress] = useState(0);
-
-  const animateProgress = useCallback(() => {
-    let progressInterval = setInterval(() => {
-      setProgress((prevProgress) =>
-        prevProgress >= props.learnt ? props.learnt : prevProgress + 1
-      );
-    }, 30);
-
-    setTimeout(() => {
-      clearInterval(progressInterval);
-    }, 3000);
-  }, [props.learnt, setProgress]);
-
-  useEffect(() => {
-    if (isComponentVisible) {
-      animateProgress();
-    }
-  }, [isComponentVisible, animateProgress]);
 
   const toggleIcon = () => {
     setSelectedIcon((prevIcon) =>
@@ -39,29 +20,26 @@ const BtnExpand = (props) => {
     setComponentVisibility((isComponentVisible) =>
       !isComponentVisible
     );
-    setProgress(0);
-    animateProgress();
   };
 
   return (
-    <li className="content-list__item">
+    <div className="content-list__item">
       <h2 className="title-2">
         {props.title} <button onClick={toggleIcon}>{selectedIcon}</button>
       </h2>
       {isComponentVisible && (
         <div>
           <p>{props.skills}</p>
-          <div className="progress-container">
-            <progress
-              className="progress-bar"
-              value={progress}
-              max="100"
-            ></progress>
-            <p className="progress-label, title-3">{progress}%</p>
+          <div class="skill-box">
+            <div class="skill-bar">
+              <span class="skill-per" style ={{width: `${props.learnt}%`}}>
+                <span class="tooltip">{props.learnt}%</span>
+              </span>
+            </div>
           </div>
         </div>
       )}
-    </li>
+    </div>
   );
 };
 
